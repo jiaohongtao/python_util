4ca8cd5bb1a966eb25b87ba919dd6d9150453061
jiaohongtao <jiaohongtao@beyondcent.com>
2020-09-18 11:33:48 +0800

    you-get添加指定目录参数

--- util/two_util/bilibili_video.py
+++ util/two_util/bilibili_video.py
@@ -9,6 +9,7 @@ https://blog.csdn.net/minge89/article/details/108414176
 
 下载单个：you-get https://www.bilibili.com/video/BV1NJ411J79W
 下载所有：you-get https://www.bilibili.com/video/BV1NJ411J79W --playlist
+指定目录：you-get -o E:/movie https://www.bilibili.com/video/BV1NJ411J79W
 """
 
 def download(url, path):
